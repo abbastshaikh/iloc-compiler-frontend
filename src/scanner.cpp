7c826b6c1ac16388f98c183f3313e10a37f31cbb
Abbas Shaikh <abts5280@gmail.com>
2025-09-15 11:13:58 -0500

    cleanup scanner

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,7 +1,6 @@
 #include <Scanner.hpp>
 #include <Token.hpp>
 #include <Opcode.hpp>
-#include <LinkedList.hpp>
 #include <iostream>
 #include <fstream>
 
@@ -15,59 +14,3 @@ Scanner::Scanner(const std::string filename) {
 Token Scanner::nextToken() {
     return Token(Category::CAT_EOF, -1);
 }
-
-// int c = 0;
-
-// LinkedList<Token> tokenList;
-// tokenList.append(Token(Category::CAT_ARITHOP, (int) Opcode::ADD));
-// tokenList.append(Token(Category::CAT_REGISTER, 1));
-// tokenList.append(Token(Category::CAT_COMMA, -1));
-// tokenList.append(Token(Category::CAT_REGISTER, 2));
-// // tokenList.append(Token(Category::CAT_INTO, -1));
-// tokenList.append(Token(Category::CAT_REGISTER, 3));
-// tokenList.append(Token(Category::CAT_EOL, -1));
-
-// tokenList.append(Token(Category::CAT_ARITHOP, (int) Opcode::ADD));
-// tokenList.append(Token(Category::CAT_REGISTER, 1));
-// tokenList.append(Token(Category::CAT_COMMA, -1));
-// tokenList.append(Token(Category::CAT_REGISTER, 2));
-// tokenList.append(Token(Category::CAT_INTO, -1));
-// tokenList.append(Token(Category::CAT_REGISTER, 3));
-// tokenList.append(Token(Category::CAT_EOL, -1));
-
-// tokenList.append(Token(Category::CAT_MEMOP, (int) Opcode::LOAD));
-// tokenList.append(Token(Category::CAT_REGISTER, 1));
-// tokenList.append(Token(Category::CAT_INTO, -1));
-// // tokenList.append(Token(Category::CAT_REGISTER, 3));
-// tokenList.append(Token(Category::CAT_EOL, -1));
-
-// tokenList.append(Token(Category::CAT_EOL, -1));
-// tokenList.append(Token(Category::CAT_EOL, -1));
-
-// // tokenList.append(Token(Category::CAT_LOADI, (int) Opcode::LOADI));
-// tokenList.append(Token(Category::CAT_CONSTANT, 12));
-// tokenList.append(Token(Category::CAT_INTO, -1));
-// tokenList.append(Token(Category::CAT_REGISTER, 3));
-// tokenList.append(Token(Category::CAT_EOL, -1));
-
-// tokenList.append(Token(Category::CAT_NOP, (int) Opcode::NOP));
-// tokenList.append(Token(Category::CAT_EOL, -1));
-
-// // tokenList.append(Token(Category::CAT_NOP, (int) Opcode::NOP));
-// tokenList.append(Token(Category::CAT_EOL, -1));
-
-// tokenList.append(Token(Category::CAT_EOF, -1));
-
-// Node<Token>* current = tokenList.head;
-// for (int i = 0; i < c; i ++) {
-//     current = current -> next;
-// }
-// c++; 
-// return current -> data;
-
-// char ch;
-// while(file.get(ch)){
-//     std::cout << ch;
-// }
-// Token token (Category::CAT_NOP, (int) Opcode::NOP);
-// return token;
